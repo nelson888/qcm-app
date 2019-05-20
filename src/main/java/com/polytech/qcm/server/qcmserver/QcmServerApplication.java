465a0bc39df1a12d9bc394805ada09d494802b52
nelson888 <tambue@hotmail.fr>
2019-05-20 09:22:49 +0200

    second commit

---
+++ src/main/java/com/polytech/qcm/server/qcmserver/QcmServerApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.qcm.server.qcmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QcmServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polytech/qcm/server/qcmserver/QcmServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.polytech.qcm.server.qcmserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QcmServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
