6227c3b58194f2a2065b4d50a7c19d76cfdd334f
nelson888 <tambue@hotmail.fr>
2019-05-21 14:20:37 +0200

    tried another thing

--- src/main/java/com/polytech/qcm/server/qcmserver/configuration/SecurityConfiguration.java
+++ src/main/java/com/polytech/qcm/server/qcmserver/configuration/SecurityConfiguration.java
@@ -16,6 +16,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Base64;
 
@@ -28,6 +31,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Autowired
   private JwtTokenProvider jwtTokenProvider;
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+          .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+      }
+    };
+  }
 
   @Bean
   @Override
