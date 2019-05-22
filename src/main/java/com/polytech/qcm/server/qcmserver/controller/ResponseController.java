ec1215ce33d05b29d35b3d2b287be9ad9c782b31
nelson888 <tambue@hotmail.fr>
2019-05-22 07:03:55 +0200

    renamed response endpoint

--- src/main/java/com/polytech/qcm/server/qcmserver/controller/ResponseController.java
+++ src/main/java/com/polytech/qcm/server/qcmserver/controller/ResponseController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-@RestController("/reponse")
+@RestController("/response")
 public class ResponseController {
   private final ChoiceRepository choiceRepository;
   private final UserRepository userRepository;
