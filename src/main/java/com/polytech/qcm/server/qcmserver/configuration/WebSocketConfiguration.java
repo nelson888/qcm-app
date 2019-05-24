1dcc337cf4e4d8acea398418fa1dd8c11062d6b1
nelson888 <tambue@hotmail.fr>
2019-05-24 02:04:04 +0200

    tried new thing

--- src/main/java/com/polytech/qcm/server/qcmserver/configuration/WebSocketConfiguration.java
+++ src/main/java/com/polytech/qcm/server/qcmserver/configuration/WebSocketConfiguration.java
@@ -1,37 +1,41 @@
 package com.polytech.qcm.server.qcmserver.configuration;
 
-import com.polytech.qcm.server.qcmserver.websocket.MessageHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.polytech.qcm.server.qcmserver.websocket.HttpHandshakeInterceptor;
+import com.polytech.qcm.server.qcmserver.websocket.SocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import java.util.concurrent.CopyOnWriteArraySet;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
-  @Override
   public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-    MessageHandler studentsSessions = new MessageHandler(new CopyOnWriteArraySet<>());
-    MessageHandler teachersSessions = new MessageHandler(new CopyOnWriteArraySet<>());
-    registry.addHandler(studentsSessions, "/auth/session/student");
-    registry.addHandler(teachersSessions, "/auth/session/teacher");
+    registry.addHandler(new SocketHandler(), "/greeting");
   }
 
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker("/topic/", "/queue/");
+    registry.setApplicationDestinationPrefixes("/app");
+  }
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    // the endpoint for websocket connections
-    registry.addEndpoint("/auth/session/student").setAllowedOrigins("*").withSockJS();
+    registry.addEndpoint("/greeting")
+      .addInterceptors(new HttpHandshakeInterceptor())
+      .setAllowedOrigins("*");
   }
+
 
 }
