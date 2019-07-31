b4fdf3294b4a303257bf257de4e58632a2b8b1ad
nelson888 <tambue@hotmail.fr>
2019-07-31 09:03:54 +0200

    added icon for qcm state

--- web-app/src/components/qcmlist.tsx
+++ web-app/src/components/qcmlist.tsx
@@ -1,5 +1,11 @@
 import React,{Component} from "react";
-import {Qcm} from "../types";
+import {Qcm, QcmState} from "../types";
+import './qcmlist.scss';
+
+const finishedIcon = require('../images/icons/finished.png');
+const incompleteIcon = require('../images/icons/incomplete.png');
+const startedIcon = require('../images/icons/ongoing.png');
+const completeIcon = require('../images/icons/complete.png');
 
 type Props = {
     qcms: Qcm[]
@@ -9,7 +15,7 @@ class QcmList extends Component<Props, {}> {
     render(): React.ReactElement {
         const {qcms} = this.props;
         return (
-            <ul className="list-group">
+            <ul>
                 {
                     qcms.map(this.toComponent)
                 }
@@ -19,11 +25,41 @@ class QcmList extends Component<Props, {}> {
 
     toComponent = (qcm: Qcm): React.ReactElement => {
         return (
-            <li key={qcm.id.toString()} className="no-margin" style={{margin: 0}}>
-                <h3>{qcm.name}</h3>
-                <p>{qcm.state}</p>
+            <li key={qcm.id.toString()} className="no-margin no-padding"
+                style={{
+                    height: 40
+                }}
+            >
+                <h3
+                    className="no-padding no-margin inline"
+                >{qcm.name}</h3>
+
+                <img
+                    className="inline"
+                    style={{
+                        width: 32,
+                        height: 32,
+                        verticalAlign: "middle"
+                    }}
+                    alt={qcm.state.toLowerCase()}
+                    src={this.getIcon(qcm.state)}
+                />
             </li>
         );
+    };
+
+    private getIcon(state: QcmState): string {
+        switch (state) {
+            default:
+            case "COMPLETE":
+                return completeIcon;
+            case "FINISHED":
+                return finishedIcon;
+            case "INCOMPLETE":
+                return incompleteIcon;
+            case "STARTED":
+                return startedIcon;
+        }
     }
 }
 
